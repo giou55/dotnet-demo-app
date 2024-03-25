04e313081e5541040132d6b1815720945588051b
GiorgosG <giorgos.gioumertakis@bel.gr>
2024-03-26 00:10:41 +0200

    changes

--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -13,6 +13,23 @@ namespace Entities
 
             modelBuilder.Entity<Country>().ToTable("Countries");
             modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //seed to Countries
+            string countriesJson = File.ReadAllText("countries.json");
+
+            List<Country> countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            foreach (Country country in countries)
+                modelBuilder.Entity<Country>().HasData(country);
+            
+
+            //seed to Countries
+            string personsJson = File.ReadAllText("persons.json");
+
+            List<Person> persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJson);
+
+            foreach (Person person in persons)
+                modelBuilder.Entity<Person>().HasData(person);
         }
     }   
 }
--- dotnet-demo-app/Program.cs
+++ dotnet-demo-app/Program.cs
@@ -6,9 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Environment
 //builder.Configuration
 
+//var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<PersonsDbContext>(options => {
-    options.UseSqlServer(); 
+    options.UseSqlServer(connectionString); 
 });
+
 
 var app = builder.Build();
 
